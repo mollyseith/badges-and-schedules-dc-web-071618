cf15e5ba383c5528b97254027d28a40b1b3bb253
Molly Seither <molly.seither@centre.edu>
2018-07-12 20:04:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,8 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  for each_char { |x| in array return "Hello, name name is #{name}."
+  end }
+end
